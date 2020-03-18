4ec41584e95317ef01921698e454237231c6f65d
miscuff <alexandrecuffel@gmail.com>
2020-03-18 14:55:32 +0100

    config Google key

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ GOOGLE_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 WIKI_SEARCH_URL = "https://fr.wikipedia.org/w/api.php"
 
 #GOOGLE_API_KEY = "AIzaSyBj4HRPq7T3yGCakocZC6JuVsL21TZX_gQ"
-GOOGLE_API_KEY = ""
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
