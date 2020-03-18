ab9f93cefcd53cfd9cc1e326cebaf952edd845d8
miscuff <alexandrecuffel@gmail.com>
2020-03-18 14:50:16 +0100

    modify config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
+import os
+
 # Configurations for the Website
 GOOGLE_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 WIKI_SEARCH_URL = "https://fr.wikipedia.org/w/api.php"
 
 #GOOGLE_API_KEY = "AIzaSyBj4HRPq7T3yGCakocZC6JuVsL21TZX_gQ"
-
-GOOGLE_API_KEY = {{ "Replace by your Google API Key" }}
-
+GOOGLE_API_KEY = ""
